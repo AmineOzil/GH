ac4ed50ac5e7716ba478d9b818d1edf992622843
Simon Probook <Simon Probook@LITE>
2017-02-24 19:34:57 +0100

    login db

---
+++ GH/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using System.Data.SQLite;
+
+namespace GH
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Admin = new MainWindow();
+            Admin.Show();
+            this.Close();
+
+        }
+    }
+}
