ab9abc453b31722c88df2421157186e66fee9930
Nadhir Boukhenifra <nadhirboukhenifra@hotmail.com>
2017-02-24 22:31:29 +0100

    Change Colors For Siimon hhh :)=

--- GH/Login.xaml.cs
+++ GH/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,39 @@ namespace GH
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var db = new SQLiteDatabase();
+
+                DataTable recipe;
+
+                String query = "select NAME \"Name\", DESCRIPTION \"Description\",";
+                query += "PREP_TIME \"Prep Time\", COOKING_TIME \"Cooking Time\"";
+
+                query += "from Login;";
+
+                recipe = db.GetDataTable(query);
+                // The results can be directly applied to a DataGridView control
+                
+                
+                // Or looped through for some other reason
+                foreach (DataRow r in recipe.Rows)
+                {
+                    MessageBox.Show(r["Name"].ToString());
+                    MessageBox.Show(r["Description"].ToString());
+                    MessageBox.Show(r["Prep Time"].ToString());
+                    MessageBox.Show(r["Cooking Time"].ToString());
+                }
+
+                
+            }
+            catch (Exception fail)
+            {
+                String error = "The following error has occurred:\n\n";
+                error += fail.Message.ToString() + "\n\n";
+                MessageBox.Show(error);
+                this.Close();
+            }
 
         }
     }
